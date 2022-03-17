2db0bf157211837409a69252fa7c6fc590a8c0ac
abo3adel <abo3adel35@gmail.com>
2022-03-17 10:57:49 +0100

    add navigaion bar

--- resources/views/layouts/guest-nav.blade.php
+++ resources/views/layouts/guest-nav.blade.php
@@ -1,73 +1,89 @@
 <!-- Navigation Start -->
-<div
-    class="fixed z-50 w-full text-gray-200 transition bg-blue-700 dark:bg-gray-800 ltr:left-0 rtl:right-0"
->
-    <div
-        class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8"
-    >
+<div class="fixed z-50 w-full transition bg-blue-700 dark:bg-gray-800 text-gray-200" x-ref="nav">
+    <div x-data="{ open: false }"
+        class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
         <div class="flex flex-row items-center justify-between p-4">
-            <a
-                href="/"
+            <a href="/"
                 class="text-lg font-semibold tracking-widest text-white whitespace-pre rounded-lg focus:outline-none focus:shadow-outline"
-                style="font-variant: small-caps"
-            >
-                {$t('nav.portfolio')}
-            </a>
-            <button
-                class="rounded-lg md:hidden focus:outline-none focus:shadow-outline"
-                on:click|preventDefault={() => {
-                    opend = !opend;
-                }}
-            >
-                <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
-                    {#if opend}
-                        <path
-                            transition:fade
-                            fill-rule="evenodd"
-                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                            clip-rule="evenodd"
-                        />
-                    {:else}
-                        <path
-                            transition:fade
-                            fill-rule="evenodd"
-                            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                            clip-rule="evenodd"
-                        />
-                    {/if}
+                style="font-variant: small-caps">{{ __('nav.portfolio') }}</a>
+            <button class="rounded-lg md:hidden focus:outline-none focus:shadow-outline"
+                x-on:click.prevent="open = !open">
+                <svg fill="currentColor" viewbox="0 0 20 20" class="w-6 h-6">
+                    <path x-show="!open" fill-rule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                        clip-rule="evenodd"></path>
+                    <path x-show="open" fill-rule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clip-rule="evenodd"></path>
                 </svg>
             </button>
         </div>
-        <nav
-            class="flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-center md:flex-row {opend
-                ? 'flex bg-blue-700 dark:bg-gray-800'
-                : 'hidden bg-[transparent]'}"
-        >
-            <!-- darkMode toggler -->
-            <DarkModeToggler addonClassList="md:hidden" />
-
-            {#each navLinks as { icon, txt }}
-                <a
-                    class="px-4 py-2 mt-2 text-sm font-semibold capitalize bg-transparent rounded-lg focus:outline-none focus:shadow-outline md:mt-0 md:ml-4 {$tabStore ===
-                    txt
-                        ? 'text-white bg-blue-800/50 dark:bg-gray-700/60 dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 active'
-                        : 'dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200  hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200'}"
-                    href={'#' + txt}
-                    use:scrollto={`#${txt}`}
-                    on:click|preventDefault={() => {
-                        $tabStore = txt;
-                    }}
-                >
-                    <i class="fas fa fa-{icon}" />
-                    <span>{$t(`nav.${txt}`)}</span>
+        <nav :class="{'flex bg-blue-700 dark:bg-gray-800': open, 'hidden bg-[transparent]': !open}"
+            class="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-center md:flex-row">
+            <a class="md:hidden px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg !text-center dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline capitalize"
+                href="#" x-on:click.prevent="darkMode = !darkMode">
+                <svg class="w-5 h-5" x-show="darkMode" aria-label="Apply light theme" role="image" fill="currentColor"
+                    viewbox="0 0 20 20">
+                    <path fill-rule="evenodd"
+                        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                        clip-rule="evenodd"></path>
+                </svg>
+                <svg style="display: none" class="w-5 h-5" x-show="!darkMode" aria-label="Apply dark theme" role="image"
+                    fill="currentColor" viewbox="0 0 20 20">
+                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                </svg>
+            </a>
+            @foreach (['news', 'tutorials'] as $link)
+                <a class="px-4 py-2 mt-2 text-sm font-semibold capitalize bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline {{Route::currentRouteName() === $link ? 'text-gray-900 bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 active' : ''}}"
+                href="{{redirect()->route($link)}}">
+                    <i class="fas fa fa-blog"></i>
+                    <span>{{ __('nav.'. $link) }}</span>
                 </a>
-            {/each}
-
-            <!-- language switcher -->
-            <LangSelector />
+            @endforeach
+            
+            <div x-on:click.away="open = false" class="relative" x-data="{ open: false }">
+                <button x-on:click.prevent="open = !open"
+                    class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left capitalize bg-transparent rounded-lg dark:bg-transparent dark:focus:text-white dark:hover:text-white dark:focus:bg-gray-600 dark:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+                    <i class="fas fa fa-language"></i>
+                    <span>{{ __('nav.language') }}</span>
+                    <svg fill="currentColor" viewbox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}"
+                        class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
+                        <path fill-rule="evenodd"
+                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </button>
+                <div x-show="open" x-transition:enter="transition ease-out duration-100"
+                    x-transition:enter-start="transform opacity-0 scale-95"
+                    x-transition:enter-end="transform opacity-100 scale-100"
+                    x-transition:leave="transition ease-in duration-75"
+                    x-transition:leave-start="transform opacity-100 scale-100"
+                    x-transition:leave-end="transform opacity-0 scale-95"
+                    class="absolute right-0 w-full mt-2 origin-top-right bg-gray-700 rounded-md shadow-lg md:w-48">
+                    <div class="px-2 py-2 bg-blue-700 rounded-md shadow dark:shadow-black dark:bg-gray-800">
+                        @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                            <a rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}" <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline {{ LaravelLocalization::getCurrentLocale() === $localeCode ? 'bg-green-700 hover:bg-teal-500 focus:bg-teal-500 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:bg-teal-700' : '' }}"
+                            {{ LaravelLocalization::getCurrentLocale() === $localeCode ? 'x-on:click.prevent' : '' }}>
+                                {{ $properties['native'] }}
+                            </a>
+                    @endforeach
+                    </div>
+                </div>
+            </div>
         </nav>
-        <DarkModeToggler addonClassList="hidden md:inline-block" />
-
-        <!-- <h1 class="border rounded hover:bg-red-600" on:click|preventDefault="{() => darkMode.set(!$darkMode)}">gh</h1> -->
+        <a class="hidden md:inline-block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg !text-center dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline capitalize"
+            href="#" x-on:click.prevent="darkMode = !darkMode">
+            <svg class="w-5 h-5" x-show="darkMode" aria-label="Apply light theme" role="image" fill="currentColor"
+                viewbox="0 0 20 20">
+                <path fill-rule="evenodd"
+                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                    clip-rule="evenodd"></path>
+            </svg>
+            <svg x-cloak class="w-5 h-5" x-show="!darkMode" aria-label="Apply dark theme" role="image"
+                fill="currentColor" viewbox="0 0 20 20">
+                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+            </svg>
+        </a>
     </div>
 </div>
+<!-- Navigation Ends -->
